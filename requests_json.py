aa49ae334bdf24e0c7cb696357b94998eef41f6a
ZhangJC2021 <94225329+Zhangjunchi2021@users.noreply.github.com>
2025-11-09 18:14:35 +0800

    服务端文件

---
+++ requests_json.py
@@ -0,0 +1,77 @@
+# get news in szzx
+# and return data in json
+
+
+class get_analyze_json():
+    
+    def __init__(self, root_url, directory, params):
+        self.root_url = root_url
+        self.url = root_url + directory
+        self.params = params
+
+        self.my_requests_get()
+        self.bs_analyze()
+        self.json_dumps()
+
+    def my_requests_get(self):
+
+        import requests
+        
+        try:
+            response = requests.get(url=self.url, params=self.params)
+            if response.status_code !=200:
+                raise Exception(f"status_code is {response.status_code} not 200")
+
+        except Exception as e:
+            raise 
+
+        response.encoding = "utf-8"
+        content = response.text
+        
+        self.content = content
+
+
+    def bs_analyze(self):
+
+        import bs4
+
+        soup = bs4.BeautifulSoup(self.content, "html.parser")
+        news = soup.find("ul", attrs={"class":"listsbox"}).find_all("a")
+
+        self.news = news
+                
+
+    def json_dumps(self):
+
+        import json
+        
+        nodes = []
+        Id = 0
+        for data in self.news:
+            node = {"id":Id,
+                    "href":f"{self.root_url}{data['href']}",
+                    "text":data.get_text()}
+            nodes.append(node)
+            Id += 1
+        
+        self.json = json.dumps(nodes)
+
+
+
+
+def main():
+
+    root_url = "https://www.szzx1000.cn"
+    direction = "/e/action/ListInfo/index.php"
+    params = {"page":"1",
+               "classid":"1"}
+    
+    szzx = get_analyze_json(root_url, direction, params)
+    print(szzx.json)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ server_update.py
@@ -0,0 +1,44 @@
+# everyday update server data at 2 a.m.
+
+
+import time
+import schedule
+
+
+def update_data():
+    
+    from datetime import datetime
+    from requests_json import get_analyze_json
+
+    print(f"datetime.now():Start update")
+    for i in range(3):
+        with open(f"page{i}.json", "w") as fp:
+            fp.write(get_analyze_json("https://www.szzx1000.cn",
+                                      "/e/action/ListInfo/index.php",
+                                      {"page":str(i),
+                                       "classid":"1"}).json)
+    
+def check_first_run():
+    import os
+    
+    if "page0.json" in os.listdir(os.getcwd()):
+        return True
+    else:
+        return False
+        
+
+def main():
+
+    if check_first_run():
+        update_data()
+    
+    schedule.every().day.at("02:00").do(update_data)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
+    
+
+
+if __name__ == "__main__":
+    main()
